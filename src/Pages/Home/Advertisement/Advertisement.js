215be76e445709495a5f14fb0ead42a42a2cdf4d
atique-atq <atique.atq@gmail.com>
2022-11-28 01:22:46 +0600

    added advertisement

--- src/Pages/Home/Advertisement/Advertisement.js
+++ src/Pages/Home/Advertisement/Advertisement.js
@@ -16,20 +16,25 @@ const Advertisement = () => {
     if (isLoading) {
         return <Loading></Loading>
     }
-    // refetch();
+    refetch();
 
     return (
         <section className='mt-28'>
-            <p className='font-mono text-4xl text-center font-bold text-[#F6740A]'>Advertisements</p>
-            <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-20 my-8'>
-                {
-                    advertisements?.map((singleAdvertisement, index) => <SingleAdvertisement
-                        key={singleAdvertisement._id}
-                        valueForBgSelection={index}
-                        singleAdvertisement={singleAdvertisement}
-                    ></SingleAdvertisement>)
-                }
-            </div>
+            {
+                (advertisements?.length > 0) &&
+                <div>
+                    <p className='font-mono text-4xl text-center font-bold text-[#F6740A]'>Advertisements</p>
+                    <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-20 my-8'>
+                        {
+                            advertisements?.map((singleAdvertisement, index) => <SingleAdvertisement
+                                key={singleAdvertisement._id}
+                                valueForBgSelection={index}
+                                singleAdvertisement={singleAdvertisement}
+                            ></SingleAdvertisement>)
+                        }
+                    </div>
+                </div>
+            }
         </section>
     );
 };
